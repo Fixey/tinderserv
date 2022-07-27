f77b15fc2158b640b1ff2c9ad04ea0a7c4cefe08
Maksim Dunaev <DunaevMaxD@gmail.com>
2022-07-27 21:43:15 +0300

    Delete ConstantsUtil.java

--- src/main/java/tinder/tindesrv/constants/ConstantsUtil.java
+++
@@ -1,5 +0,0 @@
-package tinder.tindesrv.constants;
-
-public class ConstantsUtil {
-    public final static String PICTURE_URL = "https://pict-serv-2-0-fixey-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/pict";
-}
